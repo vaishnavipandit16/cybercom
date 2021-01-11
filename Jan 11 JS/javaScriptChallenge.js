853f9425bd140d83dee78fb4ce0ac42579b38117
vaishnavipandit16 <67839180+vaishnavipandit16@users.noreply.github.com>
2021-01-11 18:45:45 +0530

    added jan 11 code

---
+++ Jan 11 JS/javaScriptChallenge.js
@@ -0,0 +1,93 @@
+//JS Coding Challenge 2
+
+//John's Information
+var johnTeam1 = 89;
+var johnTeam2 = 120;
+var johnTeam3 = 103;
+var avgScoreOfJohn;
+
+//Mike's Information
+var mikeTeam1 = 116;
+var mikeTeam2 = 94;
+var mikeTeam3 = 123;
+var avgScoreOfMike;
+
+//Mary's Information
+var maryTeam1 = 97;
+var maryTeam2 = 134;
+var maryTeam3 = 105;
+var avgScoreOfMary;
+
+//Average Score of John
+avgScoreOfJohn = (johnTeam1 + johnTeam2 + johnTeam3) / 3;
+console.log(`Average score of John is : ${avgScoreOfJohn}`);
+
+//Average score of Mike
+avgScoreOfMike = (mikeTeam1 + mikeTeam2 + mikeTeam3) / 3;
+console.log(`Average score of Mike is : ${avgScoreOfMike}`);
+
+//Average Score of Mary
+avgScoreOfMary = (maryTeam1 + maryTeam2 + maryTeam3) / 3;
+console.log(`Average score of Mary is : ${avgScoreOfMary}`);
+
+//Check the conditions foe who is winner using switch
+switch (true) {
+  case avgScoreOfJohn > avgScoreOfMike && avgScoreOfJohn > avgScoreOfMary:
+    console.log(`The winner is John. Score is  ${avgScoreOfJohn}`);
+    break;
+
+  case avgScoreOfMike > avgScoreOfJohn && avgScoreOfMike > avgScoreOfMary:
+    console.log(`The winner is Mike. Score is  ${avgScoreOfMike}`);
+    break;
+
+  case avgScoreOfMary > avgScoreOfJohn && avgScoreOfMary > avgScoreOfMike:
+    console.log(`The winner is Mary. Score is  ${avgScoreOfMary}`);
+    break;
+
+  case avgScoreOfJohn == avgScoreOfMike &&
+    avgScoreOfJohn != avgScoreOfMary &&
+    avgScoreOfJohn > avgScoreOfMary:
+    console.log(
+      `John and Mike have same scores and they are winners. Scores are ${avgScoreOfJohn} and ${avgScoreOfMike}`
+    );
+    break;
+
+  case avgScoreOfJohn == avgScoreOfMike &&
+    avgScoreOfJohn != avgScoreOfMary &&
+    avgScoreOfJohn < avgScoreOfMary:
+    console.log(`The winner is Mary. Score is  ${avgScoreOfMary}.`);
+    break;
+
+  case avgScoreOfJohn == avgScoreOfMary &&
+    avgScoreOfJohn != avgScoreOfMike &&
+    avgScoreOfJohn > avgScoreOfMike:
+    console.log(
+      `John and Mary have same scores and they are winners. Scores are ${avgScoreOfJohn} and ${avgScoreOfMary}`
+    );
+    break;
+
+  case avgScoreOfJohn == avgScoreOfMary &&
+    avgScoreOfJohn != avgScoreOfMike &&
+    avgScoreOfJohn < avgScoreOfMike:
+    console.log(`The winner is Mike. Score is  ${avgScoreOfMike}`);
+    break;
+
+  case avgScoreOfMike == avgScoreOfMary &&
+    avgScoreOfMike != avgScoreOfJohn &&
+    avgScoreOfMike > avgScoreOfJohn:
+    console.log(
+      `Mike and Mary have same scores and they are winners. Scores are ${avgScoreOfMike} and ${avgScoreOfMary}`
+    );
+    break;
+
+  case avgScoreOfMike == avgScoreOfMary &&
+    avgScoreOfMike != avgScoreOfJohn &&
+    avgScoreOfMike < avgScoreOfJohn:
+    console.log(`The winner is John. Score is  ${avgScoreOfJohn}`);
+    break;
+
+  default:
+    console.log(
+      `It's a draw.All have score ${avgScoreOfJohn} , ${avgScoreOfMike} and ${avgScoreOfMary}`
+    );
+}
